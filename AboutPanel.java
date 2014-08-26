5c0bf02f8e91dfd0d6a7c0aaf088a1fa84e82b9f
Vivek Mangla <vivekFuneesh@users.noreply.github.com>
2014-08-26 19:24:27 +0530

    Create AboutPanel.java

---
+++ AboutPanel.java
@@ -0,0 +1,49 @@
+
+/**********************
+  *************************
+        @author::Vivek Mangla
+**************************
+*****************************/
+
+/*******************************************************************
+
+AboutPanel.java is part of Champu_N_Chaandni.
+
+    Champu_N_Chaandni is free Game: you can redistribute it    and/or modify
+    it under the terms of the GNU General Public License as published     by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Champu_N_Chaandni is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Champu_N_Chaandni.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************
+*/
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class AboutPanel extends JPanel {
+    
+   
+   public AboutPanel(){
+    
+       
+       
+   }
+    
+    
+    @Override
+    public void paintComponent(Graphics g){super.paintComponent(g);
+    
+    
+   
+    }
+    
+}
